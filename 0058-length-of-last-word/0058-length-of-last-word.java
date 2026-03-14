759d3e6f5f445f2c0040e6ece548267cbb403445
Prajwal <prajwalmahamuni@gmail.com>
2026-03-14 16:58:07 +0530

    Time: 0 ms (100%), Space: 42.9 MB (91.49%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        s = s.trim();              // remove trailing spaces
+        int length = 0;
+
+        for(int i = s.length() - 1; i >= 0; i--) {
+            if(s.charAt(i) == ' ')
+                break;
+            length++;
+        }
+
+        return length;
+    }
+}
