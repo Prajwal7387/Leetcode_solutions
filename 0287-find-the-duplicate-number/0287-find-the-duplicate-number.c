56525ec092d0e12c097d822e58a10cd39a3aabec
Prajwal <prajwalmahamuni@gmail.com>
2026-03-14 16:43:30 +0530

    Time: 4 ms (56.27%), Space: 14.7 MB (30.45%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.c
@@ -0,0 +1,13 @@
+int findDuplicate(int* nums, int numsSize) {
+    int hash[numsSize+1];
+    for(int i=0;i<numsSize;i++)
+    {
+        hash[i]=0;
+    }
+    for(int i=0;i<numsSize;i++)
+    {
+        hash[nums[i]]++;
+        if(hash[nums[i]]==2){return nums[i];}
+    }
+    return -1;
+}
