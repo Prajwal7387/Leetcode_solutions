c6dd19c9a79ec1018cc1295b203b5e745fbe2f28
Prajwal <prajwalmahamuni@gmail.com>
2026-03-14 16:38:49 +0530

    Time: 4 ms (18.47%), Space: 10.9 MB (5%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.c
@@ -0,0 +1,16 @@
+char* removeDuplicates(char* s) {
+  int len = strlen(s);
+  char* stack =(char*)malloc((len + 1) *sizeof(char));  
+    
+    int top = -1;
+
+    for (int i=0;i< len;i++){
+        if(top>=0 && stack[top] ==s[i]){
+            top--;
+        }else{
+            stack[++top] =s[i];
+        }
+    }
+    stack[top+1]='\0';
+    return stack;
+}
