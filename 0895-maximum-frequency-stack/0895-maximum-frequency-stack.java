ab7fd60a3dc97703fb7429307ba1e6fd924cc7f7
Prajwal <prajwalmahamuni@gmail.com>
2026-09-09 16:10:32 +0530

    Time: 33 ms (62.26%), Space: 73.3 MB (19.41%) - LeetHub

---
+++ 0895-maximum-frequency-stack/0895-maximum-frequency-stack.java
@@ -0,0 +1,30 @@
+class FreqStack {
+    HashMap<Integer, Integer> freq = new HashMap<>();
+    HashMap<Integer, Stack<Integer>> group = new HashMap<>();
+    int maxFreq = 0;
+
+    public FreqStack() {
+    }
+
+    public void push(int val) {
+        int f = freq.getOrDefault(val, 0) + 1;
+        freq.put(val, f);
+
+        maxFreq = Math.max(maxFreq, f);
+
+        group.putIfAbsent(f, new Stack<>());
+        group.get(f).push(val);
+    }
+
+    public int pop() {
+        int val = group.get(maxFreq).pop();
+
+        freq.put(val, freq.get(val) - 1);
+
+        if (group.get(maxFreq).isEmpty()) {
+            maxFreq--;
+        }
+
+        return val;
+    }
+}
